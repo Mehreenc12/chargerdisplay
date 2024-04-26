ef884c1a82abad0a24256d0fd45b445869b05c9b
Mehreenc12 <145286375+Mehreenc12@users.noreply.github.com>
2024-04-26 02:17:51 -0500

    Update MainDisplayView.cpp

--- MainDisplayView.cpp
+++ MainDisplayView.cpp
@@ -20,7 +20,17 @@ void MainDisplayView::tearDownScreen()
 void MainDisplayView::updateCharge(){
 
     while(HAL_FDCAN_GetRxMessage(&hfdcan1, FDCAN_RX_FIFO0, &RxHeader, RxData)==HAL_OK){
-        
+        id = RxHeader.Identifier;
+		if(id == 0x223) {//ccs info
+		  vol_req = (int16_t)RxData[0];
+		  curr_req = (int16_t)RxData[2];
+		  vol_prov = (int16_t)RxData[4];
+		  curr_prov = (int16_t)RxData[6];
+		}
+		if(id == 0x220) {//pack status
+		  time_seconds = (uint8_t)RxData[0];
+		  time_minutes = (uint8_t)RxData[1];
+		}
     }    
     
 }
