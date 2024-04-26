f96ad5f08f4dad143cfd6067524403f3df61ed22
Mehreenc12 <145286375+Mehreenc12@users.noreply.github.com>
2024-04-26 01:02:19 -0500

    Create MainDisplayView.cpp

---
+++ MainDisplayView.cpp
@@ -0,0 +1,16 @@
+#include <gui/maindisplay_screen/MainDisplayView.hpp>
+
+MainDisplayView::MainDisplayView()
+{
+
+}
+
+void MainDisplayView::setupScreen()
+{
+    MainDisplayViewBase::setupScreen();
+}
+
+void MainDisplayView::tearDownScreen()
+{
+    MainDisplayViewBase::tearDownScreen();
+}
