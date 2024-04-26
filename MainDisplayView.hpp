803c2be0bd0a289bb986b3c3061a7b32ace03f02
Mehreenc12 <145286375+Mehreenc12@users.noreply.github.com>
2024-04-26 01:42:31 -0500

    Update MainDisplayView.hpp

--- MainDisplayView.hpp
+++ MainDisplayView.hpp
@@ -3,16 +3,32 @@
 
 #include <gui_generated/maindisplay_screen/MainDisplayViewBase.hpp>
 #include <gui/maindisplay_screen/MainDisplayPresenter.hpp>
+#include <texts/TextKeysAndLanguages.hpp>
 
 #include <fdcan.h>
 
 class MainDisplayView : public MainDisplayViewBase
 {
 public:
+
+    volatile uint32_t id;
+    FDCAN_RxHeaderTypeDef RxHeader;
+    uint8_t RxData[8];
+
+    int16_t vol_req;
+    int16_t curr_req;
+    int16_t vol_prov;
+    int16_t curr_prov;
+    uint16_t hvsoc;
+    uint8_t max_temp;
+
+
+
     MainDisplayView();
     virtual ~MainDisplayView() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+    virtual void updateCharge();
 protected:
 };
 
